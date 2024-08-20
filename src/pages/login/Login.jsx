d4cb7c99409a1c096a44fc75b1d3f91c05c5d4c5
Bujar Roka <rokabujar@gmail.com>
2024-08-20 20:49:33 +0200

    Added login page design

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,31 @@
-import "./login.scss"
+import "./login.scss";
 
 const Login = () => {
   return (
     <div className="login">
-        <div className="card"></div>
+      <div className="card">
+        <div className="left">
+          <h1>Hello world!</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. At
+            voluptate unde facilis ab numquam asperiores fugiat, consectetur
+            harum aliquid adipisci atque minima consequuntur aut quam excepturi,
+            eius perspiciatis quisquam tempore!
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+          </form>
+          <button>Login</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
