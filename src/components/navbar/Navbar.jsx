a85e03924bd6e3bf37602499447c8996ecacc542
Bujar Roka <rokabujar@gmail.com>
2024-08-21 22:41:21 +0200

    starting with navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,10 +7,36 @@ import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { Link } from "react-router-dom";
+
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <div>
+      <div className="navbar">
+        <div className="left">
+          <Link to="/" style={{textDecoration: "none"}}>
+          <span>Ubt Social</span>
+          </Link>
+          <HomeOutlinedIcon/>
+          <DarkModeOutlinedIcon/>
+          <GridViewOutlinedIcon/>
+          <div className="search">
+            <SearchOutlinedIcon/>
+            <input type="text" placeholder="Search"/>
+          </div>
+          </div> 
+        <div className="right">
+          <PersonOutlineOutlinedIcon/>
+          <EmailOutlinedIcon/>
+          <NotificationsOutlinedIcon/>
+          <div className="user">
+            {/* <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" /> */}
+            <span>John Doe</span>
+          </div>
+        </div>
+        </div>
+    </div>
   )
 }
 
