55006d61f73ad0b05ef115d891c3c3e44bb74694
Bujar Roka <rokabujar@gmail.com>
2024-08-21 22:50:09 +0200

    added navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -31,7 +31,7 @@ const navbar = () => {
           <EmailOutlinedIcon/>
           <NotificationsOutlinedIcon/>
           <div className="user">
-            {/* <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" /> */}
+            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
             <span>John Doe</span>
           </div>
         </div>
