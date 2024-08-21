5e173fbc3051db48e964df5c34ab7f683dcc8138
Bujar Roka <rokabujar@gmail.com>
2024-08-21 23:33:23 +0200

    added the assest folder

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -8,6 +8,10 @@ const LeftBar = () => {
             <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
             <span>John Doe</span>
           </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Friends</span>
+          </div>
         </div>
       </div>
       
