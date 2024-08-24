a3b64f3a664c2d50bb7ab3e11b9249943b03ebfc
Bujar Roka <rokabujar@gmail.com>
2024-08-24 14:46:35 +0200

    light to dark mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,20 @@ import {
   Outlet,
   Navigate,
 } from "react-router-dom";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
 function App() {
 
 
   const currentUser = true;
 
+  const {darkMode} = useContext(DarkModeContext)
+
+  console.log(darkMode)
+
   const Layout = () => {
     return (
-      <div className="theme-dark">
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
         <Navbar />
         <div style={{ display: "flex" }}>
           <LeftBar />
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,32 +10,44 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import { Link } from "react-router-dom";
 
+import { useContext } from "react";
+import { DarkModeContext } from "../../context/darkModeContext";
+
 
 const navbar = () => {
+
+
+  const { toggle, darkMode } = useContext(DarkModeContext);
+
   return (
-      <div className="navbar">
-        <div className="left">
-          <Link to="/" style={{textDecoration: "none"}}>
-          <span>UbtSocial</span>
-          </Link>
-          <HomeOutlinedIcon/>
-          <DarkModeOutlinedIcon/>
-          <GridViewOutlinedIcon/>
-          <div className="search">
-            <SearchOutlinedIcon/>
-            <input type="text" placeholder="Search"/>
-          </div>
-          </div> 
-        <div className="right">
-          <PersonOutlineOutlinedIcon/>
-          <EmailOutlinedIcon/>
-          <NotificationsOutlinedIcon/>
-          <div className="user">
-            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-            <span>John Doe</span>
-          </div>
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span >UbtSocial</span>
+        </Link>
+        <HomeOutlinedIcon />
+        {darkMode ? (
+          <WbSunnyOutlinedIcon onClick={toggle} />) :
+
+          (<DarkModeOutlinedIcon onClick={toggle} />
+
+          )}
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search" />
         </div>
+      </div>
+      <div className="right">
+        <PersonOutlineOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+          <span>John Doe</span>
         </div>
+      </div>
+    </div>
   )
 }
 
