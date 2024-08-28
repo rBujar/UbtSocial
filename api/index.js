81b3aaf8d0c0c7355d896148cf761ab8918d556f
Bujar Roka <rokabujar@gmail.com>
2024-08-29 00:35:41 +0200

    starting with the login/logout & register functions

--- api/index.js
+++ api/index.js
@@ -1,11 +1,26 @@
-import Express  from "express";
+import express  from "express";
 
-const app = Express()
+const app = express()
+import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import likeRoutes from "./routes/likes.js";
+import commentRoutes from "./routes/comments.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+//middelewars
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 
 
 
 app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/comments", commentRoutes)
+app.use("/api/likes", likeRoutes)
+app.use("/api/auth", authRoutes)
 
 app.listen(8800, ()=>{
     console.log("API working")
