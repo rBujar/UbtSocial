ceca46bf3379afc4158c72485c4e79547efbe430
Bujar Roka <rokabujar@gmail.com>
2024-09-02 00:01:37 +0200

    added the like function

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -6,16 +6,46 @@ import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
 import Comments from "../comments/Comments";
-import { useState } from "react";
-import  moment  from "moment";
+import { useContext, useState } from "react";
+import moment from "moment";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { makeRequest } from '../../axios.js'
+import { AuthContext } from "../../context/authContext.jsx";
+
 
 const Post = ({ post }) => {
 
-  const [commentOpen, setCommentOpen] = useState(false)
+  const [commentOpen, setCommentOpen] = useState(false);
 
-  //Temporary
-  const liked = false;
+  const { currentUser } = useContext(AuthContext)
 
+  const { isPending, error, data } = useQuery({
+    queryKey: ['likes', post.id],
+    queryFn: () =>
+
+      makeRequest.get("/likes?postId=" + post.id).then((res) => {
+        return res.data;
+      })
+
+  })
+
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation({
+    mutationFn: (liked) => {
+      if (liked) return makeRequest.delete("/likes?postId=" + post.id);
+      return makeRequest.post("/likes", { postId: post.id });
+    },
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ['likes'] })
+    },
+  })
+
+  const handleLike = () => {
+    mutation.mutate(data.includes(currentUser.id))
+
+  };
   return (
     <div className="post">
       <div className="container">
@@ -27,7 +57,7 @@ const Post = ({ post }) => {
               <Link to={`/profile/${post.id}`} style={{ textDecoration: "none", color: "inherit" }}>
                 <span className="name">{post.name}</span>
               </Link>
-                <span className="date">{moment(post.createdAt).fromNow()}</span>
+              <span className="date">{moment(post.createdAt).fromNow()}</span>
             </div>
           </div>
           <MoreHorizIcon />
@@ -38,8 +68,14 @@ const Post = ({ post }) => {
         </div>
         <div className="info">
           <div className="item">
-            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
-            12 Likes
+            {isPending ? "Pending..." : data.includes(currentUser.id) ? (
+              <FavoriteOutlinedIcon
+                style={{ color: "red" }} onClick={handleLike} />
+            )
+              : (<FavoriteBorderOutlinedIcon onClick={handleLike} />
+
+              )}
+            {data?.length} Likes
           </div>
           <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
             <TextsmsOutlinedIcon />
@@ -50,7 +86,7 @@ const Post = ({ post }) => {
             12 Shared
           </div>
         </div>
-        {commentOpen && <Comments postId={post.id}/>}
+        {commentOpen && <Comments postId={post.id} />}
       </div>
     </div>
   )
